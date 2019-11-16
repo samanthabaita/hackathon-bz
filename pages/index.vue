43636d9e2a68a473813187da3c44077efd052f56
Samantha Baita <baita.samantha@gmail.com>
2019-11-16 02:54:17 +0100

    Add images and contact info

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,37 @@
 <template>
   <div>
-    <section class="hero">
+    <section class="hero is-large" :style="`backgroundImage: url('/img/1.jpg')`">
       <div class="hero-body">
         <div class="container has-text-centered">
-          <h1 class="title is-1">
+          <h1 class="title is-1 has-text-white">
             Vieni a trovarci
           </h1>
         </div>
       </div>
     </section>
-    <section class="hero is-primary is-success is-medium">
-      <div class="hero-body">
-        <div class="container">
-          Qui ci sarà il web component
-        </div>
+    <section class="section">
+      <div class="container">
+        <h2 class="title is-2">Contact Info</h2>
+        <nav class="level">
+  <div class="level-item has-text-centered">
+    <div>
+      <p class="heading">Address</p>
+      <p class="title">Address fake</p>
+    </div>
+  </div>
+  <div class="level-item has-text-centered">
+    <div>
+      <p class="heading">Phone</p>
+      <p class="title">1234567890</p>
+    </div>
+  </div>
+  <div class="level-item has-text-centered">
+    <div>
+      <p class="heading">Email</p>
+      <p class="title"><a href="mailto:email@example.org">email@example.org</a></p>
+    </div>
+  </div>
+</nav>
       </div>
     </section>
   </div>
