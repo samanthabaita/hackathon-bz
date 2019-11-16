742505f7e45f9b7746c5d0cdd4ef963e942979bf
Samantha Baita <baita.samantha@gmail.com>
2019-11-16 02:57:39 +0100

    Add fake example data

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <a class="navbar-item" href="https://bulma.io">
-        Hotel DEMO
+        Hotel Kronenhof
       </a>
 
       <a
--- pages/index.vue
+++ pages/index.vue
@@ -3,35 +3,35 @@
     <section class="hero is-large" :style="`backgroundImage: url('/img/1.jpg')`">
       <div class="hero-body">
         <div class="container has-text-centered">
-          <h1 class="title is-1 has-text-white">
-            Vieni a trovarci
-          </h1>
+          <h1 class="title is-1 has-text-white">Vieni a trovarci</h1>
         </div>
       </div>
     </section>
     <section class="section">
       <div class="container">
-        <h2 class="title is-2">Contact Info</h2>
+        <h2 class="title is-3" style="margin-bottom: 3rem,">Contact Info</h2>
         <nav class="level">
-  <div class="level-item has-text-centered">
-    <div>
-      <p class="heading">Address</p>
-      <p class="title">Address fake</p>
-    </div>
-  </div>
-  <div class="level-item has-text-centered">
-    <div>
-      <p class="heading">Phone</p>
-      <p class="title">1234567890</p>
-    </div>
-  </div>
-  <div class="level-item has-text-centered">
-    <div>
-      <p class="heading">Email</p>
-      <p class="title"><a href="mailto:email@example.org">email@example.org</a></p>
-    </div>
-  </div>
-</nav>
+          <div class="level-item has-text-centered">
+            <div>
+              <p class="heading">Address</p>
+              <p class="title is-4">Corso della Libertà, 117, 39100 Bolzano BZ 047 2992030</p>
+            </div>
+          </div>
+          <div class="level-item has-text-centered">
+            <div>
+              <p class="heading">Phone</p>
+              <p class="title is-4">1234567890</p>
+            </div>
+          </div>
+          <div class="level-item has-text-centered">
+            <div>
+              <p class="heading">Email</p>
+              <p class="title is-4">
+                <a href="mailto:email@example.org">email@example.org</a>
+              </p>
+            </div>
+          </div>
+        </nav>
       </div>
     </section>
   </div>
